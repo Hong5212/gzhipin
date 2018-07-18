0dd2b53d3f0df1db1675f485e24a07283f8f9c57
Hong5212 <1262940993@qq.com>
2018-07-18 21:37:22 +0800

    'laobanInfo'

--- gzhipin_client/src/components/header-selector/header-selector.jsx
+++ gzhipin_client/src/components/header-selector/header-selector.jsx
@@ -1,9 +1,22 @@
 import React, {Component} from 'react'
 import {List, Grid} from 'antd-mobile'
+import PropTypes from 'prop-types'
 
 export default class HeaderSelector extends Component{
-    render(){
 
+    static propTypes = {
+        setHeader: PropTypes.func,isRequired
+    }
+
+    state = {
+        icon: null
+    }
+
+    selectHeader = ({text, icon}) => {
+        this.props.setHeader(text)
+    }
+
+    render(){
         const headerList = [];
         for(var i = 0; i < 20; i++){
             const text = '头像' + (i + 1);
